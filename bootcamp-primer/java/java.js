c8dc3fce5d4437bf8ff694d507898de2873c455f
Kosai-Al-Ghanom <kosai.alghanom@powercoders.org>
2020-03-27 13:27:20 +0100

    removing the commentline that i don't need

--- bootcamp-primer/java/java.js
+++ bootcamp-primer/java/java.js
@@ -1,10 +1,4 @@
-//We have to create For Loop. I declared that the variable begins from 1 and so long the variable is less
-// or equal to 100 the increment is 1 every time
-//The first condition, if remainder division the variable by 7 and 5 at the same time is equal to 0. 
-// Print the string “Marko polo". 
-//Then check if remainder division the variable by 7 is equal to 0. Print the string “polo". 
-//Then check if remainder division the variable by 5 is equal to 0. Print the string “Marko". 
-//Other than that, print the variable itself
+
 
 
 for (var i=1; i<=100; i++){
