f9f4faa7df7467e11f8b43dbf7b7a4220072d381
Kosai-Al-Ghanom <kosai.alghanom@powercoders.org>
2020-03-29 15:32:35 +0200

    add javascript files

---
+++ bootcamp-primer/javascript/palindrome.js
@@ -0,0 +1,11 @@
+
+
+
+
+var string="otto";
+if (string == string.split('').reverse().join('')) {
+    console.log(string + ' is palindrome.');
+}
+else {
+    console.log(string + ' is not palindrome.');
+}
